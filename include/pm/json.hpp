3e9d3393d2fa656fbef2f879cf7c3d5d7122890a
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2022-06-02 07:26:50 +0200

    JSON keys for phase data

---
+++ include/pm/json.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+namespace pm {
+
+/**
+ * \brief The key for the child array in a \ref Phase JSON document
+ */
+constexpr char const* JSON_KEY_CHILDREN = "children";
+
+/**
+ * \brief The key for the data object in a \ref Phase JSON document
+ */
+constexpr char const* JSON_KEY_DATA = "data";
+
+/**
+ * \brief The key for the metrics object in a \ref Phase JSON document
+ */
+constexpr char const* JSON_KEY_METRICS = "metrics";
+
+/**
+ * \brief The key for the name in a \ref Phase JSON document
+ */
+constexpr char const* JSON_KEY_NAME = "name";
+
+}
